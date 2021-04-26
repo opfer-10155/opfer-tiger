160d2e5fc086777c343cf836df14e563cfc1c99e
= <=>
2021-04-26 20:42:35 +0900

    [Fix] this maybe correct treatment of nil types

--- semantic/type.ml
+++ semantic/type.ml
@@ -6,13 +6,26 @@ type fields = symbol * ty
 and unique = unit ref
 
 and ty =
-  | UnitTy
-  | IntTy
-  | StrTy
-  (* | NilTy *)
-  | ArrTy of ty          * unit ref [@equal fun (ArrTy (_ , u1)) (ArrTy (_ , u2)) -> u1 == u2]
-  | RecTy of fields list * unit ref [@equal fun (RecTy (_ , u1)) (RecTy (_ , u2)) -> u1 == u2]
-  | FuncTy of ty list * ty
-  | TmpTy of Ast.ty
+    | UnitTy
+    | IntTy
+    | StrTy
+    (* 任意のRecordのsubtype *)
+    | NilTy
+    | ArrTy of ty          * unit ref
+    | RecTy of fields list * unit ref
+    | FuncTy of ty list * ty
+    (*
+    * 一時的な型、型の相互再帰の検査に利用する
+    *)
+    | TmpTy of Ast.ty
 [@@deriving show, eq]
+
+
+(* 2つの型が等しいか検査する *)
+let type_match t1 t2 = match (t1 , t2) with
+    | (ArrTy (_ , u1) , ArrTy (_ , u2))
+    | (RecTy (_ , u1), RecTy (_ , u2))  ->  u1 == u2
+    | (NilTy , RecTy _)
+    | (RecTy _ , NilTy) -> true
+    | _ -> t1 == t2
 
