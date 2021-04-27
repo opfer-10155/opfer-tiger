58baa59bbc02b439d5b2565f7911b7f36d4f86c7
= <=>
2021-04-27 18:39:39 +0900

    [update] upgraded source code position information

--- semantic/position.ml
+++ semantic/position.ml
@@ -1,25 +1,48 @@
-open Lexing
-
-type pos = {
-  pos_fname : string;
-  pos_lnum : int;
-  pos_bol : int;
-  pos_cnum : int;
-}
+(* type for a location in source code *)
+type position = [%import: Lexing.position]
 [@@deriving show]
 
-let equal_pos _ _ = true
+let equal_position _ _ = true
 
-let to_pos (p:position) : pos = {
-  pos_fname=p.pos_fname;
-  pos_lnum=p.pos_lnum;
-  pos_bol=p.pos_bol;
-  pos_cnum=p.pos_cnum;
-}
+(* (start_pos , end_pos) *)
+type location = position * position
+[@@deriving show]
 
-let dummy_pos = {
-  pos_fname="dummy";
-  pos_lnum=0;
-  pos_bol=0;
-  pos_cnum=0;
-}
+let equal_location _ _ = true
+
+
+(* alias *)
+type pos = location
+[@@deriving show, eq]
+
+(* initializing *)
+
+let dummy_loc =
+  (Lexing.dummy_pos, Lexing.dummy_pos)
+
+(* alias *)
+let dummy_pos = dummy_loc
+
+let curr_loc lexbuf =
+  (Lexing.lexeme_start_p lexbuf, Lexing.lexeme_end_p lexbuf)
+
+(* Printing *)
+
+let pp_position ppf pos =
+  Format.fprintf ppf "%s:%i.%i"
+                 pos.pos_fname
+                 pos.pos_lnum
+                 (pos.pos_cnum - pos.pos_bol)
+
+let pp_location ppf (left, right) =
+  if left.pos_fname = right.pos_fname then
+    Format.fprintf ppf "%s:%i.%i-%i.%i"
+                   left.pos_fname
+                   left.pos_lnum
+                   (left.pos_cnum - left.pos_bol)
+                   right.pos_lnum
+                   (right.pos_cnum - right.pos_bol)
+  else
+    Format.fprintf ppf "%a-%a"
+                   pp_position left
+                   pp_position right
